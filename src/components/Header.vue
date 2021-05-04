51b2ea26b5c8a355eab1987c4d4969d9a6d4de07
mauriziogiuffredi91 <giuffro91@gamil.com>
2021-05-04 21:05:04 +0200

    add nav

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,9 +1,27 @@
 <template>
     <header>
-        <a href="">logo</a>
+        <div class="logo-box">
+            <a href="">logo</a>
+
+        </div>
+
+        
 
         <nav>
-            nav items
+            <ul>
+                <li>
+                    <a href=""><h4>characters</h4></a>
+                    <a href=""><h4>comics</h4></a>
+                    <a href=""><h4>movies</h4></a>
+                    <a href=""><h4>tv</h4></a>
+                    <a href=""><h4>games</h4></a>
+                    <a href=""><h4>collectibles</h4></a>
+                    <a href=""><h4>videos</h4></a>
+                    <a href=""><h4>fans</h4></a>
+                    <a href=""><h4>news</h4></a>
+                    <a href=""><h4>shop</h4></a>
+                </li>
+            </ul>
         </nav>
     </header>
 </template>
@@ -14,6 +32,24 @@ export default {
 }
 </script>
 
-<style>
+<style scoped>
+    header{
+        display: flex;
+        justify-content: space-between;
 
+    }
+    
+    nav ul li{
+        display: inline-block;
+        list-style: none;
+        
+
+
+    }
+
+    nav ul li a{
+        text-decoration: none;
+        text-transform: uppercase;
+        color: rgba(0, 0, 0, 0.835);
+    }
 </style>
