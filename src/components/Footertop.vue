dff5ab8e64eb37a15acc131267d20d50d295581b
mauriziogiuffredi91 <giuffro91@gmail.com>
2021-05-05 22:22:41 +0200

    add date footer

--- src/components/Footertop.vue
+++ src/components/Footertop.vue
@@ -6,21 +6,30 @@
 
                 <ul>
                     <li>
-                        <h4>Dc comics</h4>
+                        <h3>Dc comics</h3>
+
+                    </li>
+
+                    <li class="options" 
+                        v-for="(items, index) in dcComics" 
+                        :key="index"
+                    >
+                    <a href="/">{{items.oggetto}}</a>
+
                     </li>
 
                     <li>
-                        <h4>Shop</h4>
+                        <h3>Shop</h3>
                     </li>
                 </ul>
                 <ul>
                     <li>
-                        <h4>Dc</h4>
+                        <h3>Dc</h3>
                     </li>
                 </ul>
                 <ul>
                     <li>
-                        <h4>Sites</h4>
+                        <h3>Sites</h3>
                     </li>
                 </ul>
             </div>
@@ -38,6 +47,53 @@
 <script>
 export default {
     name: 'Footertop',
+    data() {
+        return {
+            dcComics:[
+                {oggetto: 'Characters', url: '/characters/page'},
+                {oggetto: 'Comics', url: '/comics/page'},
+                {oggetto: 'Movies', url: '/movies/page'},
+                {oggetto: 'TV', url: '/tv/page'},
+                {oggetto: 'Games', url: '/games/page'},
+                {oggetto: 'Videos', url: '/videos/page'},
+                {oggetto: 'News', url: '/news/page'},
+                
+            ],
+            
+            shop:[
+                
+                {oggetto: 'Shop Dc', url: '/shop/page'},
+                {oggetto: 'Shop Dc Collectibles', url: '/dcCollectibles/page'},
+                
+            ],
+
+            dc:[
+                {oggetto: 'Terms Of Use', url: '/termsOfUse/page'},
+                {oggetto: 'Privacy policy (New)', url: '/policy/page'},
+                {oggetto: 'Ad Choices', url: '/choicheAd/page'},
+                {oggetto: 'Advertising', url: '/advertising/page'},
+                {oggetto: 'Jobs', url: '/jobs/page'},
+                {oggetto: 'Subscriptions', url: '/subscriptions/page'},
+                {oggetto: 'Talent Workshops', url: '/talentWorkshop/page'},
+                {oggetto: 'CPSC Certificates', url: '/CPSC/page'},
+                {oggetto: 'Ratings', url: '/ratings/page'},
+                {oggetto: 'Shop Help', url: '/shop/page'},
+                {oggetto: 'Contact Us', url: '/contact/page'},
+                
+            ],
+            sites:[
+                {oggetto: 'DC', url: '/dc/page'},
+                {oggetto: 'MAD Magazine', url: '/MADmagazine/page'},
+                {oggetto: 'DC Kids', url: '/dcKids/page'},
+                {oggetto: 'DC Universe', url: '/DcUniverse/page'},
+                {oggetto: 'DC Power Visa', url: '/powerVisa/page'},
+                
+                
+            ],
+                
+        };
+    },
+    methods: {},
 }
 </script>
 
@@ -53,7 +109,29 @@ export default {
         justify-content: space-between;
     }
 
-    
+    .main-box .text-container{
+        display: flex;
+        justify-content: space-between;
+
+    }
+
+    .text-container ul{
+        width: 200px;
+        padding: 30px;
+    }
+    .text-container ul li{
+        margin-right: 20px;
+        list-style: none;
+        text-decoration: none;
+        
+
+    }
+
+    .text-container ul li h3{
+        color: ghostwhite;
+        text-transform: uppercase;
+
+    }
 
     
 
