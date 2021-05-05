2b041a524745d481d3e45193a359378ee81c5fef
mauriziogiuffredi91 <giuffro91@gmail.com>
2021-05-05 21:36:15 +0200

    correction

--- src/components/Hero.vue
+++ src/components/Hero.vue
@@ -1,15 +1,19 @@
 <template>
-    <section class="main-box">
-        <ul>
-            <li v-for="(object, index) in links" :key="index" >
-                <!-- <img :src="`object.img`" alt="">
-                 :src per inserire immagine dinamica da js ci vogliono backtik `` e inserimento 
-                <a href="/">{{object.text}}</a> 
-                -->
-            </li>
+    <section >
 
-            
-        </ul>
+        <div class="main-box">
+            <ul>
+                <li class="box" v-for="(object, index) in links" :key="index" >
+                    <img :src="object.img" alt="`${object.text}`">
+                        <!-- mancava il require -->
+                    <a href="/">{{object.text}}</a> 
+                    
+                </li>
+
+                
+            </ul>
+
+        </div>
 
     </section>
 </template>
@@ -20,11 +24,35 @@ export default {
     data() {
         return {
             links:[
-                {text: 'digital comics', url: '/digitalComics/page', current: false, img:'../assets/img/buy-comics-digital-comics.png'},
-                {text: 'dc merchandise', url: '/dcMerchandise/page', current: false, img: '../assets/img/buy-comics-merchandise.png'},
-                {text: 'subscription', url: '/subScription/page', current: false, img: '../assets/img/buy-comics-subscriptions.png'},
-                {text: 'comic shop locator', url: '/comicShopLocator/page', current: false, img: '../assets/img/buy-comics-shop-locator.png'},
-                {text: 'dc power visa', url: '/dcPowerVisa/page', current: false, img: '../assets/img/buy-dc-power-visa.png'},
+                {
+                    text: 'digital comics', 
+                    url: '/digitalComics/page', 
+                    current: false, 
+                    img: require ('../assets/img/buy-comics-digital-comics.png'),
+                },
+                {
+                    text: 'dc merchandise',
+                    url: '/dcMerchandise/page', 
+                    current: false, 
+                    img: require( '../assets/img/buy-comics-merchandise.png'),
+                },
+                {   text: 'subscription', 
+                    url: '/subScription/page', 
+                    current: false, 
+                    img: require( '../assets/img/buy-comics-subscriptions.png'),
+                },
+                {
+                    text: 'comic shop locator',
+                    url: '/comicShopLocator/page', 
+                    current: false, 
+                    img: require( '../assets/img/buy-comics-shop-locator.png'),
+                },
+                {
+                    text: 'dc power visa',
+                    url: '/dcPowerVisa/page', 
+                    current: false, 
+                    img: require( '../assets/img/buy-dc-power-visa.svg'),
+                },
                 
                 
                 
@@ -36,5 +64,50 @@ export default {
 </script>
 
 <style scoped>
+    section{
+        background-color: dodgerblue;
+    }
+    .main-box{
+        padding: 40px;
+    }
+        
+        
+
+    .main-box ul{
+        display: flex;
+        align-items: center;
+        justify-content: space-between;
+    }
+
+
+
+    .main-box ul li{
+        display: flex;
+        align-items: center;
+        list-style: none;
+        
+    }
+        
+        
+        
+        
+
+    
+
+    .main-box ul li a{
+        text-transform: uppercase;
+        color: ghostwhite;
+        text-decoration: none;
+        font-size: 15px;
+        
+
+    }
+
+    ul li img{
+        width: 45px;
+        height: 60px;
+        margin-right: 10px;
+        
+    }
 
 </style>
