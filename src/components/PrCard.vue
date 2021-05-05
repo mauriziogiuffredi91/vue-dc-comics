9aa2c66e188954b56e42cff07b94aebf4738b625
mauriziogiuffredi91 <giuffro91@gmail.com>
2021-05-05 16:40:35 +0200

    insert card

---
+++ src/components/PrCard.vue
@@ -0,0 +1,17 @@
+<template>
+    <div class="box-fumetto">
+        <img src="" alt="">
+
+        <h5>Action Comics</h5>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'PrCard',
+}
+</script>
+
+<style>
+
+</style>
