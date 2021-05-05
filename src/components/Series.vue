13f5a6d57d0742729690c145ed8a010af26037be
mauriziogiuffredi91 <giuffro91@gmail.com>
2021-05-05 15:57:34 +0200

    test riquadri css

--- src/components/Series.vue
+++ src/components/Series.vue
@@ -1,6 +1,74 @@
 <template>
     <section class="current-comics">
         <div class="main-box">
+
+            <!-- test sui riquadri -->
+            <div class="box-fumetto">
+                <img src="" alt="">
+
+                <h5>Action Comics</h5>
+            </div>
+
+            <div class="box-fumetto">
+                <img src="" alt="">
+
+                <h5>Action Comics</h5>
+            </div>
+
+            <div class="box-fumetto">
+                <img src="" alt="">
+
+                <h5>Action Comics</h5>
+            </div>
+
+            <div class="box-fumetto">
+                <img src="" alt="">
+
+                <h5>Action Comics</h5>
+            </div>
+
+            <div class="box-fumetto">
+                <img src="" alt="">
+
+                <h5>Action Comics</h5>
+            </div>
+
+            <div class="box-fumetto">
+                <img src="" alt="">
+
+                <h5>Action Comics</h5>
+            </div>
+
+            <div class="box-fumetto">
+                <img src="" alt="">
+
+                <h5>Action Comics</h5>
+            </div>
+
+            <div class="box-fumetto">
+                <img src="" alt="">
+
+                <h5>Action Comics</h5>
+            </div>
+
+            <div class="box-fumetto">
+                <img src="" alt="">
+
+                <h5>Action Comics</h5>
+            </div>
+
+            <div class="box-fumetto">
+                <img src="" alt="">
+
+                <h5>Action Comics</h5>
+            </div>
+
+            <div class="box-fumetto">
+                <img src="" alt="">
+
+                <h5>Action Comics</h5>
+            </div>
+
             <div class="box-fumetto">
                 <img src="" alt="">
 
@@ -21,17 +89,29 @@ export default {
 
     .current-comics{
         background-color: #1c1c1c;
+        padding: 54px 0;
+    }
+
+    .current-comics .main-box{
+        display: flex;
+        flex-wrap: wrap;
+        
     }
 
 
-    .box-fumetto{
-        
+    .box-fumetto img{
+        /*test riquadri*/ 
+        background-color: gray;
+        height: 180px;
+        width: 180px;
+        margin: 10px;
         
     }
 
     .box-fumetto h5{
         color: floralwhite;
         text-transform: uppercase;
+        padding-left: 10px;
     }
 
 
